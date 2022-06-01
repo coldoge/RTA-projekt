4a2d9d941226a8ab241dddc12b09e7e107e90324
janscheuring <jscheuring14@gmail.com>
2022-06-01 19:51:17 +0200

    cleaning data

---
+++ preprocessing.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import re
+
+df = pd.read_csv('data/audi_a6_1000obs.csv', index_col=0, dtype=str)
+
+print(df.isna().sum(axis=0))
+
+# wyrzucamy zmienne z wieloma brakami danych
+cols = list(df.loc[:, :'Kolor'].columns) + ['Stan']
+
+# wyrzucamy zmienne z brakami danych oraz zmienne, ktore uwazamy za nieistotne
+for zmienna in ['Kategoria', 'Marka pojazdu', 'Model pojazdu', 'Liczba drzwi', 'Liczba miejsc', 'Wersja',
+                'Spalanie W Mieście']:
+    cols.remove(zmienna)
+
+#wyrzucamy wiersze z brakami danych
+df = df[cols].dropna(axis=0)
+
+def extract_number_from_string(napis):
+    return int(re.search('[\d ]+', napis).group(0).replace(' ', ''))
+
+
+for zmienna in ['Cena', 'Rok produkcji', 'Przebieg', 'Pojemność skokowa', 'Moc']:
+    df[zmienna] = df[zmienna].apply(extract_number_from_string)
+
+for i in df.columns:
+    print(df[i].value_counts())
+
+#wyrzucamy obserwacje kategoryczne z licznoscia mniejsza od 5
+
+df = df[~df['Napęd'].isin(['Na tylne koła', '4x4 (dołączany ręcznie)'])]
+df = df[~df['Typ nadwozia'].isin(['SUV', 'Kompakt'])]
+df = df[df['Kolor']!='Bordowy']
+
+#wyrzucamy moc rowna 1, jest to oczywisty buserlad
+
+df = df[df['Moc']!=1]
+
+df = df.reset_index(drop=True)
+
+df.to_csv('data/final_audi_a6_data.csv')
